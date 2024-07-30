8907dab7958a7fff91bc85f593113d9412265125
fatima ouali <fatimaouali965@gmail.com>
2024-07-30 23:43:42 +0100

    install tailwind

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 
 export default function Home() {
-    // cont [count , setCount ] = useState(0)
+    
 
   return (
-    <div>Home</div>
+    <div>
+      home
+    </div>
   )
 }

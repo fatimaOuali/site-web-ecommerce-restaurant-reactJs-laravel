6fbacf97f7e71fed0ab93272826b70e29c99503c
fatima ouali <fatimaouali965@gmail.com>
2024-08-01 13:29:09 +0100

    install shadcn

---
+++ frontend/src/components/clients/UserLogin.jsx
@@ -0,0 +1,75 @@
+import * as z from "zod"
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "../ui/form.jsx";
+import {Input} from "../ui/input.jsx";
+import {Button} from "../ui/button.jsx";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+  email: z.string().min(2).max(50),
+  password: z.string().min(8).max(50),
+})
+
+export default function UserLogin() {
+  const form = useForm ({
+    resolver: zodResolver(formSchema),
+    defaultValues:{
+      username:'',
+    }
+  })
+ 
+  function onSubmit(values) {
+
+    console.log(values)
+  }
+  return (
+    <>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+         <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>email</FormLabel>
+              <FormControl>
+                <Input placeholder="email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+         <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>password</FormLabel>
+              <FormControl>
+                <Input type={'password'} placeholder="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+        
+      </form>
+    </Form>
+    </>
+  )
+}
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import UserLogin from "../components/clients/UserLogin.jsx";
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>
+        <UserLogin />
+
+    </div>
   )
 }
+
